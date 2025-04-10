83596f1c929dca22ac872568b547b87e65e96e49
Anuj Pandey <142150494+AnujPandey10@users.noreply.github.com>
2025-04-10 16:26:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'anujmukul9@gmail.com'        # Replace with your email
-app.config['MAIL_PASSWORD'] = 'valz inpl jlvp asyd'         # Replace with your email password
+app.config['MAIL_USERNAME'] = ''        # Replace with your email
+app.config['MAIL_PASSWORD'] = ''         # Replace with your email password
 app.config['MAIL_DEFAULT_SENDER'] = 'anujmukul9@gmail.com'
 mail = Mail(app)
 
